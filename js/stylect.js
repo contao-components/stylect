6045f51aac900a6753409c97edf6f082af59098f
Leo Feyer <github@contao.org>
2016-06-07 13:58:08 +0200

    Add a wrapper element (see contao/core-bundle#507).

--- js/stylect.js
+++ js/stylect.js
@@ -89,6 +89,7 @@ var Stylect =
 			// Clone the template
 			if (!div) {
 				div = Stylect.template.clone();
+				new Element('div.styled_select_wrapper').wraps(el); // see #507
 				div.addClass(cls).inject(el, 'before');
 			}
 
